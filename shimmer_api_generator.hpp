7a73ed443b01863bf8f1e2236724f35c9fb2a4ed
Felix <f.klinge15@gmail.com>
2021-09-06 22:58:28 +0200

    first test-iteration of type parsing

--- shimmer_api_generator.hpp
+++ shimmer_api_generator.hpp
@@ -13,40 +13,111 @@ struct ObjCTypeDict
     uint32_t           entryCapacity;
 };
 
-static uint32_t djb2_hash( const char* pString )
+struct ParseResult
+{
+    const char* pTestFilePath = nullptr;
+};
+
+ParseResult parseCommandLineArguments( const int argc, const char** argv )
+{
+    enum ParseState
+    {
+        NextArgument,
+        ParseTestArg
+    };
+
+    ParseState state = NextArgument;
+    ParseResult result;
+
+    for( int argIndex = 0u; argIndex < argc; ++argIndex )
+    {
+        switch( state )
+        {
+            case NextArgument:
+            {
+                if( strcmp( argv[argIndex], "--test" ) == 0 )
+                {
+                    state = ParseTestArg;
+                    continue;
+                }
+                break;
+            }
+
+            case ParseTestArg:
+            {
+                result.pTestFilePath = argv[argIndex];
+                state = NextArgument;
+                break;
+            }
+
+            default:
+            {
+                InvalidCodePath();
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+ObjCTypeDict* createObjectiveCTypeDictionary( size_t sizeInBytes )
+{
+    RuntimeAssert(sizeInBytes > sizeof( ObjCTypeDict ));
+    uint8_t* pDictionaryMemory = (uint8_t*)malloc( sizeInBytes );
+    ObjCTypeDict* pTypeDict = (ObjCTypeDict*)pDictionaryMemory;
+
+    pDictionaryMemory += sizeof( ObjCTypeDict );
+
+    sizeInBytes -= sizeof( ObjCTypeDict );
+
+    pTypeDict->pEntries = (ObjCTypeDictEntry*)pDictionaryMemory;
+    pTypeDict->entryCapacity = sizeInBytes / sizeof( ObjCTypeDictEntry );
+
+    //FK: mark all entries as `isNew`
+    memset( pTypeDict->pEntries, 1, sizeInBytes );
+    return pTypeDict;
+}
+
+uint8_t stringsAreEqual( const char* pStringA, const char* pStringB, size_t stringLength )
+{
+    for( size_t i = 0u; i < stringLength; ++i )
+    {
+        if( pStringA[ i ] != pStringB[ i ] )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+uint32_t djb2_hash( const char* pString, const size_t stringLength )
 {
     uint32_t hash = 5381;
 
-    for(;;)
+    for(size_t i = 0u; i < stringLength; ++i)
     {
-        if( pString == 0 )
-        {
-            break;
-        }
-
-        const char c = *pString++;
+        const char c = pString[i];
         hash = ((hash << 5) + hash) + c;
     }
 
     return hash;
 }
 
-
-
-ObjCTypeDictEntry* insertTypeDictionaryEntry( ObjCTypeDict* pDict, const char* restrict_modifier pTypeName, uint8_t* restrict_modifier pOutIsNew )
+ObjCTypeDictEntry* insertTypeDictionaryEntry( ObjCTypeDict* pDict, const char* restrict_modifier pTypeName, const size_t typeNameLength, uint8_t* restrict_modifier pOutIsNew )
 {
     //FK: TODO: Check hash distribution
-    const uint32_t hashValue = djb2_hash( pTypeName );
+    const uint32_t hashValue = djb2_hash( pTypeName, typeNameLength );
     const uint32_t entryIndex = hashValue % pDict->entryCapacity;
     ObjCTypeDictEntry* pEntry = pDict->pEntries + entryIndex;
 
     *pOutIsNew = pEntry->isNew;
     if( pEntry->isNew )
     {
-        const size_t typeNameLength = strlen( pTypeName ); 
         pEntry->originalTypeLength = typeNameLength;
 
-        //FK: TODO: use stringpool?
+        //FK: TODO: use custom linear allocator?
         pEntry->pOriginalType = (char*)malloc( typeNameLength + 1 ); 
         if( pEntry->pOriginalType == nullptr )
         {
@@ -54,17 +125,22 @@ ObjCTypeDictEntry* insertTypeDictionaryEntry( ObjCTypeDict* pDict, const char* r
             //FK: TODO: Print message, let the user know what's going on
             return nullptr;
         }
-        strcpy( pEntry->pOriginalType, pTypeName );
+        sprintf( pEntry->pOriginalType, "%.*s", (int)typeNameLength, pTypeName );
         pEntry->isNew = 0;
-
-        //FK:
     }
+
+    //FK: Check for hash collision
+    RuntimeAssert( stringsAreEqual( pEntry->pOriginalType, pTypeName, typeNameLength ) );
 
     return pEntry;
 }
 
 const char* parseObjectiveCStruct( const char* pTypeName, const size_t typeNameLength )
 {
+    char* pTemp = (char*)malloc( typeNameLength + 1 );
+    sprintf( pTemp, "%.*s", (int)typeNameLength, pTypeName );
+    return pTemp;
+
     UnusedArgument(pTypeName);
     UnusedArgument(typeNameLength);
 
@@ -73,17 +149,17 @@ const char* parseObjectiveCStruct( const char* pTypeName, const size_t typeNameL
     char* pTempMemory = (char*)alloca(1024);
     memset(pTempMemory, 0, 1024);
 
-    return nullptr;
+    return pTypeName;
 }
 
 const char* parseObjectiveCType( const char* pTypeName, const size_t typeNameLength )
 {
-    UnusedArgument(pTypeName);
-    UnusedArgument(typeNameLength);
-    return nullptr;
+    char* pTemp = (char*)malloc( typeNameLength + 1 );
+    sprintf( pTemp, "%.*s", (int)typeNameLength, pTypeName );
+    return pTemp;
 }
 
-const char* resolveObjectiveCTypeName( ObjCTypeDict* pTypeDict, const char* pTypeName, const size_t typeNameLength )
+const char* resolveObjectiveCTypeName( ObjCTypeDict* pTypeDict, const char* pTypeName, size_t typeNameLength )
 {
     if( typeNameLength == 1u )
     {
@@ -110,10 +186,12 @@ const char* resolveObjectiveCTypeName( ObjCTypeDict* pTypeDict, const char* pTyp
 
             case 'v':
                 return "void";
+
+            case ':':
+                return "nssignal_t";
         }
 
-        InvalidCodePath();
-        return "";
+        return nullptr;
     }
     else if( typeNameLength == 2u )
     {
@@ -127,15 +205,27 @@ const char* resolveObjectiveCTypeName( ObjCTypeDict* pTypeDict, const char* pTyp
     {
         //FK: Make copy to manipulate string for easier, human-readible lookup 
         char* pTypeNameCopy = (char*)alloca( typeNameLength );
-        sprintf( pTypeNameCopy, "%s", pTypeName );
+        sprintf( pTypeNameCopy, "%.*s", (int)typeNameLength, pTypeName );
         
         //FK: struct
-        pTypeNameCopy += 2; //FK: Skip '{_'
+        if( *pTypeNameCopy == '{' )
+        {
+            ++pTypeNameCopy;
+        }
+
+        if( *pTypeNameCopy == '_' )
+        {
+            ++pTypeNameCopy;
+        }
+
         char* pTypeNameCopyEnd = strstr( pTypeNameCopy, "=" );
+        RuntimeAssert( pTypeNameCopyEnd != nullptr );
         *pTypeNameCopyEnd = 0;
 
+        typeNameLength = ( pTypeNameCopyEnd - pTypeNameCopy );
+
         uint8_t isNew = 0;
-        ObjCTypeDictEntry* pTypeDictEntry = insertTypeDictionaryEntry( pTypeDict, pTypeNameCopy, &isNew );
+        ObjCTypeDictEntry* pTypeDictEntry = insertTypeDictionaryEntry( pTypeDict, pTypeNameCopy, typeNameLength, &isNew );
         if( isNew )
         {
             pTypeDictEntry->pResolvedType = parseObjectiveCStruct( pTypeName, typeNameLength );
@@ -145,11 +235,221 @@ const char* resolveObjectiveCTypeName( ObjCTypeDict* pTypeDict, const char* pTyp
     }
 
     uint8_t isNew = 0;
-    ObjCTypeDictEntry* pTypeDictEntry = insertTypeDictionaryEntry( pTypeDict, pTypeName, &isNew );
-    if( pTypeDictEntry->isNew )
+    ObjCTypeDictEntry* pTypeDictEntry = insertTypeDictionaryEntry( pTypeDict, pTypeName, typeNameLength, &isNew );
+    if( isNew )
     {
         pTypeDictEntry->pResolvedType = parseObjectiveCType( pTypeName, typeNameLength );
     }
 
     return pTypeDictEntry->pResolvedType;
+}
+
+const char* parseToAfterNextNewline( const char* pText )
+{
+    while( *pText++ != '\n' );
+    return pText;
+}
+
+const char* findNextWhitespace( const char* pText )
+{
+    while( *pText )
+    {
+        if( isspace( *pText ) )
+        {
+            break;
+        }
+
+        ++pText;
+    }
+
+    return pText;
+}
+
+const char* parseToBeforePreviousWhitespace( const char* pTextStart, const char* pText )
+{
+    while( pText != pTextStart )
+    {
+        if( isspace( *pText ) )
+        {
+            ++pText;
+            break;
+        }
+
+        --pText;
+    }
+
+    return pText;
+}
+
+void convertToLower( char* pDestination, const char* pSource, size_t sourceLength )
+{
+    for( size_t i = 0u; i < sourceLength; ++i )
+    {
+        pDestination[ i ] = tolower( pSource[i] );
+    }
+}
+
+void parseTestFile( FILE* pResultFileHandle, const char* pFileContentBuffer, size_t fileContentBufferSizeInBytes )
+{
+    constexpr size_t dictSizeInBytes = 1024*1024; //FK: 1 MiB
+    ObjCTypeDict* pDict = createObjectiveCTypeDictionary( dictSizeInBytes );
+    if( pDict == nullptr )
+    {
+        return;
+    }
+
+    //FK: 
+    const char* pClassNameEnd = strstr( pFileContentBuffer, ":" );
+    const char* pClassNameStart = parseToBeforePreviousWhitespace( pFileContentBuffer, pClassNameEnd );
+
+    const size_t classNameLength = ( pClassNameEnd - pClassNameStart );
+    char* pLowerClassName = (char*)malloc( classNameLength + 1 );
+    convertToLower( pLowerClassName, pClassNameStart, classNameLength );
+
+    pFileContentBuffer = parseToAfterNextNewline( pFileContentBuffer );
+
+    enum State
+    {
+        EatWhitespace = 0,
+        EatUntilNewLine,
+        ParseReturnType,
+        ParseArgumenType,
+        ParseFunctionName
+    };
+
+    constexpr uint8_t ArgumentsToSkip = 2;
+
+    uint8_t argumentCount = 0u;
+    State state = ParseReturnType;
+    State previousState = ParseReturnType;
+    while( *pFileContentBuffer != 0 )
+    {
+        switch( state )
+        {
+            case EatWhitespace:
+            {
+                if( *pFileContentBuffer == '\n' )
+                {
+                    fprintf( pResultFileHandle, ");\n" );
+                    argumentCount = 0u;
+                    state = ParseReturnType;
+                    ++pFileContentBuffer;
+                    break;
+                }
+                else if( !isspace( *pFileContentBuffer ) )
+                {
+                    if( previousState == ParseArgumenType ||
+                        previousState == ParseFunctionName )
+                    {
+                        state = ParseArgumenType;
+                    }                    
+                    else if( previousState == ParseReturnType )
+                    {
+                        state = ParseFunctionName;
+                    }
+                    break;
+                }
+                
+                ++pFileContentBuffer;
+                break;
+            }
+
+            case EatUntilNewLine:
+            {
+                if( *pFileContentBuffer == '\n' )
+                {
+                    ++pFileContentBuffer;
+                    state = ParseReturnType;
+                    break;
+                }
+
+                ++pFileContentBuffer;
+                break;
+            }
+
+            case ParseFunctionName:
+            {
+                const char* pNextWhiteSpacePosition = strstr( pFileContentBuffer, " " );
+                const char* pNextColonPosition = strstr( pFileContentBuffer, ":" );
+
+                //FK: Ignore function name extension naming optional arguments
+                //    eg: nextEventMatchingMask:untilDate:inMode:dequeue:
+                size_t functionNameLength = ( pNextWhiteSpacePosition - pFileContentBuffer );
+                if( pNextColonPosition < pNextWhiteSpacePosition )
+                {
+                    functionNameLength = ( pNextColonPosition - pFileContentBuffer );
+                }
+
+                //FK: Omit trailing ':' from function name;
+                if( pFileContentBuffer[ functionNameLength ] == ':' )
+                {
+                    functionNameLength -= 1;
+                }
+
+                fprintf( pResultFileHandle, "%s_%.*s(", pLowerClassName, (int)functionNameLength, pFileContentBuffer );
+
+                pFileContentBuffer += functionNameLength;
+                state = EatWhitespace;
+                break;
+            }
+
+            case ParseReturnType:
+            case ParseArgumenType:
+            {
+                const char* pNextWhiteSpacePosition = findNextWhitespace( pFileContentBuffer );
+                size_t typeLength = 0u;
+                if( pNextWhiteSpacePosition == nullptr )
+                {
+                    typeLength = strlen( pFileContentBuffer );
+                }
+                else
+                {
+                    typeLength = ( pNextWhiteSpacePosition - pFileContentBuffer );
+                }
+
+                ++argumentCount;
+                if( state == ParseArgumenType && argumentCount <= ArgumentsToSkip )
+                {
+                    state = EatWhitespace;
+                    pFileContentBuffer += typeLength;
+                    break;
+                }
+
+                const char* pResolvedType = resolveObjectiveCTypeName( pDict, pFileContentBuffer, typeLength );
+                if( pResolvedType == nullptr )
+                {
+                    printf( "Could not resolve type '%.*s'\n", (int)typeLength, pFileContentBuffer );
+                    state = EatUntilNewLine;
+                    break;
+                }
+
+                if( state == ParseArgumenType )
+                {
+                    if( argumentCount != (ArgumentsToSkip + 1) )
+                    {
+                        fprintf( pResultFileHandle, "%s", ", " );
+                    }
+
+                    fprintf( pResultFileHandle, "%s arg%d", pResolvedType, ( argumentCount - ArgumentsToSkip ) );
+                }
+                else
+                {
+                    fprintf( pResultFileHandle, "%s\t", pResolvedType );
+                }
+                
+                pFileContentBuffer += typeLength;
+                state = EatWhitespace;
+                break;
+            }
+
+            default:
+                InvalidCodePath();
+                break;
+        }
+
+        if( state != EatWhitespace )
+        {
+            previousState = state;
+        }
+    }
 }
--- win32/win32entry.cpp
+++ win32/win32entry.cpp
@@ -1,131 +1,65 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <windows.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <ctype.h>
 #include <malloc.h>
 
 #define RuntimeAssert(x) if(!(x)){ __debugbreak();}
 #define InvalidCodePath() RuntimeAssert(false)
-
-#define RestrictModifier __restrict__
-
 #define UnusedArgument(x) (void)x
 
-#pragma comment(lib, "kernel32.lib")
-#pragma comment(lib, "user32.lib")
+#define restrict_modifier __restrict__
 
 #include "..\shimmer_api_generator.hpp"
 
-typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
-
-void allocateDebugConsole()
+int main( int argc, const char** argv )
 {
-	AllocConsole();
-	AttachConsole(ATTACH_PARENT_PROCESS);
-	freopen("CONOUT$", "w", stdout);
-}
-
-LRESULT CALLBACK K15_WNDPROC(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	uint8_t messageHandled = 0;
-
-	switch (msg)
+	if( argc == 1 )
 	{
-	case WM_CREATE:
-		break;
-
-	case WM_CLOSE:
-		PostQuitMessage(0);
-		messageHandled = 1;
-		break;
-
-	case WM_KEYDOWN:
-	case WM_KEYUP:
-	case WM_SYSKEYDOWN:
-	case WM_SYSKEYUP:
-		break;
-
-	case WM_LBUTTONUP:
-	case WM_MBUTTONUP:
-	case WM_RBUTTONUP:
-	case WM_XBUTTONUP:
-	case WM_LBUTTONDOWN:
-	case WM_RBUTTONDOWN:
-	case WM_MBUTTONDOWN:
-	case WM_XBUTTONDOWN:
-		break;
-
-	case WM_MOUSEMOVE:
-		break;
-
-	case WM_MOUSEWHEEL:
-		break;
-	}
-
-	if (messageHandled == 0)
-	{
-		return DefWindowProc(hWnd, msg, wparam, lparam);
-	}
-
-	return 0;
-}
-
-HWND setupWindow(HINSTANCE p_Instance, int p_Width, int p_Height)
-{
-	WNDCLASS wndClass = {0};
-	wndClass.style = CS_HREDRAW | CS_OWNDC | CS_VREDRAW;
-	wndClass.hInstance = p_Instance;
-	wndClass.lpszClassName = "K15_Win32Template";
-	wndClass.lpfnWndProc = K15_WNDPROC;
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	RegisterClass(&wndClass);
-
-	HWND hwnd = CreateWindowA("K15_Win32Template", "Win32 Template",
-		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-		p_Width, p_Height, 0, 0, p_Instance, 0);
-
-	if (hwnd == INVALID_HANDLE_VALUE)
-		MessageBox(0, "Error creating Window.\n", "Error!", 0);
-	else
-		ShowWindow(hwnd, SW_SHOW);
-	return hwnd;
-}
-
-void setup()
-{
-	allocateDebugConsole();
-}
-
-int CALLBACK WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nShowCmd)
-{
-	LARGE_INTEGER performanceFrequency;
-	QueryPerformanceFrequency(&performanceFrequency);
-
-	HWND hwnd = setupWindow(hInstance, 1024, 768);
-
-	if (hwnd == INVALID_HANDLE_VALUE)
+		//FK: TODO: Print help
+		printf("Too few arguments.\n");
 		return -1;
-
-	setup();
-
-	uint8_t loopRunning = 1;
-	MSG msg;
-	while (loopRunning)
-	{
-		while (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE) > 0)
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-
-			if (msg.message == WM_QUIT)
-				loopRunning = 0;
-		}
-
-		Sleep(10);
 	}
 
+	ParseResult parseResult = parseCommandLineArguments( argc - 1, argv + 1 );
+	if( parseResult.pTestFilePath == nullptr )
+	{
+		printf("Real use only supported under macOS, use --test <testfile> to test implementation under win32.");
+		return -1;
+	}
+
+	FILE* pTestFileHandle = fopen( parseResult.pTestFilePath, "r" );
+	if( pTestFileHandle == nullptr )
+	{
+		printf("Couldn't open test file '%s' for reading.", parseResult.pTestFilePath );
+		return -1;
+	}
+
+	//FK: TODO: mmap file
+	fseek(pTestFileHandle, 0, SEEK_END);
+	const size_t fileSizeInBytes = ftell(pTestFileHandle);
+	fseek(pTestFileHandle, 0, SEEK_SET);
+
+	char* pFileBuffer = (char*)malloc( fileSizeInBytes );
+	if( pFileBuffer == nullptr )
+	{
+		printf("Couldn't allocate %.3fKiB as file buffer.", (float)fileSizeInBytes/1024.f);
+		return -1;
+	}
+
+	fread( pFileBuffer, 1u, fileSizeInBytes, pTestFileHandle );
+	fclose( pTestFileHandle );
+	pTestFileHandle = nullptr;
+
+	FILE* pTestOutFileHandle = fopen( "test_out.hpp", "w" );
+	if( pTestOutFileHandle == nullptr )
+	{
+		printf( "Could not open file 'test_out.hpp' for writing." );
+		return -1;
+	}
+
+	parseTestFile( pTestOutFileHandle, pFileBuffer, fileSizeInBytes );
 	return 0;
 }
